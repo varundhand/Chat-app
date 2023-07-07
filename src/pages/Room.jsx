cd1eafa5df87067cd46e6a62e98c33b68dacf255
varundhand <varundhand77@gmail.com>
2023-07-08 03:10:20 +0530

    added styling

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -16,22 +16,24 @@ const Room = () => {
   }
 
   return (
-    <div>
-      <div className="">
-        {messages.map(message => (
-          <div className="" key={message.$id}>
+    <main className='container'>
+      <div className="room--container">
+        <div className="">
+          {messages.map(message => (
+            <div key={message.$id} className='message--wrapper' >
+              <div className="message-header">
+                <small className='message-timestamp'>{message.$createdAt}</small>
+              </div>
 
-            <div className="">
-              <p>{message.$createdAt}</p>
+              <div className='message--body'>
+                <span>{message.body}</span>
+              </div>
             </div>
-
-            <div>
-              <span>{message.body}</span>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      
+    </main>
   )
 }
 
