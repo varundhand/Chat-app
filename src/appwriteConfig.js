3f24568ddc13075063f1af67d7362db696f11574
varundhand <varundhand77@gmail.com>
2023-07-08 00:11:22 +0530

    integrated appwrite

---
+++ src/appwriteConfig.js
@@ -0,0 +1,9 @@
+import { Client } from "appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(import.meta.env.VITE_SECRET);
+
+export default client;
---
+++ src/pages/Room.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Room = () => {
+  return (
+    <div>
+      ROOM
+    </div>
+  )
+}
+
+export default Room
